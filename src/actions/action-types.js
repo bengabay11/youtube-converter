8d37fd3fa75195820df82c02f2fc6219df757d70
ben <ben.gabay38@gmail.com>
2019-05-09 20:40:01 +0300

    added delete and add new song actions

--- src/actions/action-types.js
+++ src/actions/action-types.js
@@ -1,3 +1,4 @@
 export const UPDATE_LINK =  "UPDATE_LINK";
 export const UPDATE_SONG_NAME =  "UPDATE_SONG_NAME";
 export const ADD_SONG = "ADD_SONG";
+export const DELETE_SONG = "DELETE_SONG";
--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,4 +1,4 @@
-import {ADD_SONG, UPDATE_LINK} from "./action-types";
+import {ADD_SONG, DELETE_SONG, UPDATE_LINK} from "./action-types";
 
 export function updateLink(newLink) {
     return {type: UPDATE_LINK, link: newLink};
@@ -8,6 +8,10 @@ export function updateSongName(newSongName) {
     return {type: UPDATE_LINK, songName: newSongName};
 }
 
-export function addSong() {
-    return {type: ADD_SONG};
+export function addSong(name, link) {
+    return {type: ADD_SONG, name: name, link: link};
+}
+
+export function deleteSong(id) {
+    return {type: DELETE_SONG, id: id};
 }
