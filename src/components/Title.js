ed840a4d5d66a7773401c12614688815fe2886cb
ben <ben.gabay38@gmail.com>
2019-05-10 12:47:57 +0300

    remove brackets

--- src/components/Title.js
+++ src/components/Title.js
@@ -2,13 +2,11 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import "../styles/title.css"
 
-export const Title = ({content}) => {
-    return (
-        <div className="title font">
-            {content}
-        </div>
-    );
-};
+export const Title = ({content}) => (
+    <div className="title font">
+        {content}
+    </div>
+);
 
 Title.propTypes = {
     content: PropTypes.string.isRequired
