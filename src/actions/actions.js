995a5da33a24c9133d35be17101c8e06db46b688
ben <ben.gabay38@gmail.com>
2019-05-11 13:48:10 +0300

    changed the actions to arrow functions

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,26 +1,46 @@
 import {ADD_SONG, DELETE_SONG, UPDATE_FORMAT, UPDATE_LINK, UPDATE_SONG} from "./action-types";
 import {getSongInfoFromYoutube} from "../services/youtube-song-info";
-import {Song} from "../DTOs/Song";
 
-export function updateLink(newLink) {
-    return {type: UPDATE_LINK, link: newLink};
-}
+export const updateLink = (newLink) => {
+    return {
+        type: UPDATE_LINK,
+        link: newLink
+    };
+};
 
-export function updateFormat(newFormat) {
-    return {type: UPDATE_FORMAT, format: newFormat};
-}
+export const updateFormat = (newFormat) => {
+    return {
+        type: UPDATE_FORMAT,
+        format: newFormat
+    };
+};
 
 export function addSong(link, format) {
-    let songInfo = getSongInfoFromYoutube(link);
-    let newSong = Song(songInfo.id, songInfo.title, link, format, songInfo.artist,
-        songInfo.uploadedAt, songInfo.duration);
-    return {type: ADD_SONG, song: newSong};
+    // getSongInfoFromYoutube(link).then(songInfo => {
+    //     let newSong = Song(songInfo.id, songInfo.title, link, format, songInfo.artist,
+    //         songInfo.uploadedAt, songInfo.duration);
+    //     return {
+    //         type: ADD_SONG,
+    //         song: newSong
+    //     };
+    // });
+    return {
+        type: ADD_SONG
+    };
 }
 
-export function deleteSong(id) {
-    return {type: DELETE_SONG, id: id};
-}
+export const deleteSong = (id) => {
+    return {
+        type: DELETE_SONG,
+        id: id
+    };
+};
 
-export function updateSong(id, field, value) {
-    return {type: UPDATE_SONG, id: id, field: field, value: value};
-}
+export const updateSongField = (id, field, value) => {
+    return {
+        type: UPDATE_SONG,
+        id: id,
+        field: field,
+        value: value
+    };
+};
