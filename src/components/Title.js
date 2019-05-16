c13a802b83759224c0953dbd51f0e0321ec113a3
ben <ben.gabay38@gmail.com>
2019-05-17 01:28:21 +0300

    added icon to the title

--- src/components/Title.js
+++ src/components/Title.js
@@ -3,8 +3,9 @@ import PropTypes from 'prop-types'
 import "../styles/title.css"
 
 export const Title = ({content}) => (
-    <div className="title font">
+    <div className="title font row-center">
         {content}
+        <img src={ require('../assets/icon.jpg') } alt="Icon" className="icon-image"/>
     </div>
 );
 
