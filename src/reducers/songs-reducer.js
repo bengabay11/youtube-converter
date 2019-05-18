919dcc8ccf96532bbbe9c6d8a4e9860d4e4e40ea
ben <ben.gabay38@gmail.com>
2019-05-18 17:36:09 +0300

    change the state itself to the list of songs

--- src/reducers/songs-reducer.js
+++ src/reducers/songs-reducer.js
@@ -1,27 +1,20 @@
 import {ADD_SONG, DELETE_SONG, UPDATE_SONG} from "../actions/action-types";
 
-let initialState = {
-    songs: []
-};
-
-function songs(state = initialState, action) {
-    let newSongs = [];
+function songs(state = [], action) {
     switch (action.type) {
         case ADD_SONG:
-            return {...state, songs: state.songs.concat(action.song)};
+            return [...state.concat(action.song)];
         case DELETE_SONG:
-            newSongs = [...state.songs].filter(song => {
+            return [...state].filter(song => {
                 return song.id !== action.id;
             });
-            return {...state, songs: newSongs};
         case UPDATE_SONG:
-            newSongs = [...state.songs].map(song => {
+            return [...state.songs].map(song => {
                 if (song.id === action.id) {
                     song[action.field] = action.value;
                 }
                 return song;
             });
-            return {...state, songs: newSongs};
         default:
             return state;
     }
