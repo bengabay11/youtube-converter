69b3357f7d0cdfe8da5414ba0f1265b6fd2134aa
ben <ben.gabay38@gmail.com>
2019-05-18 17:34:38 +0300

    added conteiner for songs component

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,13 @@ import React from "react"
 import Title from "./Title"
 import config from "../config"
 import AddSong from "../containers/AddSong"
+import SongsProvider from "../containers/SongsProvider";
 
 export const App = () => (
     <div>
         <Title content={config.titleContent}/>
         <AddSong/>
+        <SongsProvider/>
     </div>
 );
 
