3418fcceafdb719bff35b068cb153650e1e800b4
ben <ben.gabay38@gmail.com>
2019-06-03 19:44:58 +0300

    added http responses

--- server/config.js
+++ server/config.js
@@ -5,6 +5,10 @@ const config = {
     app: {
         port: process.env['PORT'] || 3001,
         staticFolder: process.env['STATIC_FOLDER'] || 'client/build'
+    },
+    httpResponses : {
+        ok: 200,
+        notFound: 404
     }
 };
 
