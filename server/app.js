e24edfe4456bd245ac7326264c537008cd5699f2
ben <ben.gabay38@gmail.com>
2019-06-23 21:08:49 +0300

    when applied, the server serve the build folder of the client successfully

--- server/app.js
+++ server/app.js
@@ -9,10 +9,10 @@ const printRequest = require('./middlewares/printRequest');
 const app = express();
 
 app.use(printRequest);
-app.use('/', routes);
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, config.app.staticFolder)));
+app.use(express.static(path.join(__dirname, "../", config.app.staticFolder)));
+app.use('/', routes);
 
 app.listen(config.app.port, () => console.log(`Listening on port ${config.app.port}...`));
--- server/routes/index.js
+++ server/routes/index.js
@@ -7,8 +7,8 @@ router.use('/videos', require('./videos'));
 router.use('/health_check', require('./health_check'));
 
 // Handle React routing, return all requests to React app
-router.get('*', (req, res) => {
-    let pagePath = path.join(__dirname, config.app.staticFolder, 'index.html');
+router.get('/', (req, res) => {
+    let pagePath = path.join(__dirname, "../../", config.app.staticFolder, 'index.html');
     if (fs.existsSync(pagePath)) {
         res.sendFile(pagePath);
     }
