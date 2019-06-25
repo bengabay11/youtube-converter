62c2c8fd188caecbe6107501ff9c6ab9fecd9ad2
ben <ben.gabay38@gmail.com>
2019-06-25 17:42:09 +0300

    removed comment

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,7 +6,6 @@ const fs = require('fs');
 router.use('/videos', require('./videos'));
 router.use('/health_check', require('./health_check'));
 
-// Handle React routing, return all requests to React app
 router.get('/', (req, res) => {
     let pagePath = path.join(__dirname, "../../", config.app.buildFolder, 'index.html');
     if (fs.existsSync(pagePath)) {
