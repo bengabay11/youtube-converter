f345262f872178f94a44274c312c6487925ea170
ben <ben.gabay38@gmail.com>
2020-03-19 01:14:27 +0200

    router files uses the new config

--- server/routes/videos/index.js
+++ server/routes/videos/index.js
@@ -4,15 +4,16 @@ const ytdl = require('ytdl-core');
 const youtubedl = require('youtube-dl');
 const httpStatus = require('http-status-codes');
 
-router.get('/info', (req, res) => {
-    let videoLink = req.query["link"];
+router.get(config.resources.videoInfo, (req, res) => {
+    const videoId = req.params["video_id"];
+    const videoLink = config.video_url + videoId;
     ytdl.getInfo(videoLink,{}, (err, info) => {
         if (err) {
-            let response = {
+            let responseBody = {
                 message: `Error accrued while fetching info about the video: ${videoLink}`,
                 error: err
             };
-            res.status(httpStatus.INTERNAL_SERVER_ERROR).send(response);
+            res.status(httpStatus.INTERNAL_SERVER_ERROR).send(responseBody);
         }
         else {
             info["uploaded_at"] = new Date(info["published"]).toLocaleDateString();
@@ -20,18 +21,19 @@ router.get('/info', (req, res) => {
             info["formats"] = info["formats"]
                 .filter((format, index) => info["formats"].indexOf(format) === index && format !== undefined);
             info["duration"] = new Date(info["length_seconds"] * 1000).toISOString().substr(11, 8);
-            info["channel_url"] = `${config.channel_url}/${info["ucid"]}`;
+            info["channel_url"] = `${config.youtube.channel_url}/${info["ucid"]}`;
             res.status(httpStatus.OK).send(info);
         }
     });
 });
 
-router.get('/download', (req, res) => {
-    let videoLink = req.query["link"];
-    let videoName = req.query["name"];
-    let format = req.query["format"];
-    let options = [`--format=${format}`];
-    let video = youtubedl(videoLink, [], options);
+router.get(config.resources.downloadVideo, (req, res) => {
+    const videoId = req.params["video_id"];
+    const videoLink = config.video_url + videoId;
+    const videoName = req.query["name"];
+    const format = req.query["format"];
+    const options = [`--format=${format}`];
+    const video = youtubedl(videoLink, [], options);
     try {
         res.set('Content-Disposition', `attachment; filename="${videoName}.${format}"`);
         video.pipe(res);
