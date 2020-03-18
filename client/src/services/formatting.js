ba11bd2bef35e4421affd5576719541bad8b1452
ben <ben.gabay38@gmail.com>
2020-03-19 01:41:41 +0200

    delete method generate resource url from formatting service

--- client/src/services/formatting.js
+++ client/src/services/formatting.js
@@ -5,11 +5,6 @@ export const formatString = (string, params) => {
     return string;
 };
 
-export const generateResourceUrl = (host, path, ...values) => {
-    const resource = formatString(path, values);
-    return host + resource;
-};
-
 export const getVideoIdFromLink = (videoLink) => {
     return videoLink.split("?v=")[1];
 };
