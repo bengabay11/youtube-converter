3bcdc6b3179387edca4a365cc9526030040c885d
ben <ben.gabay38@gmail.com>
2020-03-19 01:13:24 +0200

    add resouces and youtube urls to config

--- server/config.js
+++ server/config.js
@@ -8,7 +8,16 @@ const config = {
         buildFolder: process.env['BUILD_FOLDER'] || 'client/build',
         assetsFolder: process.env['ASSETS_FOLDER'] || 'client/public/assets'
     },
-    channel_url: "https://www.youtube.com/channel"
+    resources: {
+        healthCheck: "/health_check",
+        videos: "/videos",
+        videoInfo: "/:video_id/info",
+        downloadVideo: "/:video_id/download"
+    },
+    youtube: {
+        channel_url: "https://www.youtube.com/channel",
+        video_url: "https://www.youtube.com/watch?v="
+    }
 };
 
 module.exports = config;
