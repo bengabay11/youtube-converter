9d74e4179feacd4b834659ab9b9a20739117ac3b
ben <ben.gabay38@gmail.com>
2020-03-19 16:33:16 +0200

    add new utility - formatting

---
+++ server/utils/formatting.js
@@ -0,0 +1,15 @@
+export const formatResponseBodyBySongInfo = songInfo => {
+    return {...songInfo,
+        uploaded_at: new Date(songInfo.published).toLocaleDateString(),
+        formats: getFormatContainersByFormats(songInfo.formats),
+        duration: convertSecondsToTime(songInfo.length_seconds),
+        channel_url: `${config.youtube.channel_url}/${songInfo.ucid}`
+    };
+};
+
+export const convertSecondsToTime = seconds => new Date(seconds * 1000).toISOString().substr(11, 8);
+
+export const getFormatContainersByFormats = formats => {
+    formats = formats.map(format => format.container);
+    return formats.filter((format, index) => formats.indexOf(format) === index && format !== undefined);
+};
