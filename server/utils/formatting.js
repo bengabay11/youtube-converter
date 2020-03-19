c37221c7d62c7a000502764bf6e693fd500bf86d
ben <ben.gabay38@gmail.com>
2020-03-19 17:00:17 +0200

    remove export statemetns and add module.exports in formatting file in server utils

--- server/utils/formatting.js
+++ server/utils/formatting.js
@@ -1,4 +1,4 @@
-export const formatResponseBodyBySongInfo = songInfo => {
+const formatResponseBodyBySongInfo = songInfo => {
     return {...songInfo,
         uploaded_at: new Date(songInfo.published).toLocaleDateString(),
         formats: getFormatContainersByFormats(songInfo.formats),
@@ -7,9 +7,13 @@ export const formatResponseBodyBySongInfo = songInfo => {
     };
 };
 
-export const convertSecondsToTime = seconds => new Date(seconds * 1000).toISOString().substr(11, 8);
+const convertSecondsToTime = seconds => new Date(seconds * 1000).toISOString().substr(11, 8);
 
-export const getFormatContainersByFormats = formats => {
+const getFormatContainersByFormats = formats => {
     formats = formats.map(format => format.container);
     return formats.filter((format, index) => formats.indexOf(format) === index && format !== undefined);
+};
+
+module.exports = {
+    formatResponseBodyBySongInfo
 };
