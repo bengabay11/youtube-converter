726ae2f14a6cfaa57a07a0df439c3c4a6eaf4b9f
ben <ben.gabay38@gmail.com>
2020-03-19 12:45:46 +0200

    delet project for niv's cr

--- server/middlewares/printRequest.js
+++
@@ -1,9 +0,0 @@
-module.exports = (req, res, next) => {
-    res.on('finish', () => {
-        const date = new Date();
-        const currentTime = `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
-        const requestMessage = `* ${currentTime} [${req.method}] ${req.originalUrl} ${res.statusCode}`
-        console.log(requestMessage);
-    });
-    next();
-};
--- server/routes/health_check/index.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const httpStatus = require('http-status-codes');
-
-
-router.get('/', (req, res) => {
-    res.status(httpStatus.OK).send("The connection to the server has been successfully completed")
-});
-
-module.exports =router;
