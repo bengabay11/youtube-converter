d1ef1f01d201a3cb000b2cacf287ba54e89467a5
ben <ben.gabay38@gmail.com>
2020-03-20 18:55:17 +0200

    add new method to formatting service - paramsObjectToQueryString

--- client/src/services/formatting.js
+++ client/src/services/formatting.js
@@ -5,4 +5,17 @@ export const formatString = (string, params) => {
     return string;
 };
 
-export const getVideoIdFromLink = (videoLink) => videoLink.split("?v=")[1];
+export const paramsObjectToQueryString = paramsObject => {
+    let query = "";
+    if (paramsObject){
+        const firstParamKey = Object.keys(paramsObject)[0];
+        query += `?${firstParamKey}=${paramsObject[firstParamKey]}`;
+        delete paramsObject[firstParamKey];
+        for (const [key, value] of Object.entries(paramsObject)) {
+            query += `&${key}=${value}`;
+        }
+    }
+    return query;
+};
+
+export const getVideoIdFromLink = videoLink => videoLink.split("?v=")[1];
