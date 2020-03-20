f91e1ba5865206777948b155104ec50b78a330e6
ben <ben.gabay38@gmail.com>
2020-03-20 20:28:08 +0200

    add exception handling to ajax sevice

--- client/src/services/ajax.js
+++ client/src/services/ajax.js
@@ -1,12 +1,18 @@
 import {paramsObjectToQueryString} from "./formatting";
 
-export const sendHttpRequest = async (url, method, body, params, headers={}) => {
-        url += paramsObjectToQueryString(params);
-        return await fetch(url, {
-            method,
-            headers,
-            body
-        });
+export const sendHttpRequest = async (url, method, body, params, headers={}, callbackError) => {
+        try{
+            url += paramsObjectToQueryString(params);
+            return await fetch(url, {
+                method,
+                headers,
+                body
+            });
+        }
+        catch (e) {
+            callbackError(e);
+        }
+
 };
 
 export const handleResponse = async (response, statusOptions) => {
